c260895d4d5793a5dc0bc1b8773dc229b8dac814
Dainf <dainf@utfpr.edu.br>
2017-07-28 20:08:05 -0300

    primeiro commit

---
+++ myMemories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myMemories
+//
+//  Created by dainf on 28/07/17.
+//  Copyright © 2017 br.utfpr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
